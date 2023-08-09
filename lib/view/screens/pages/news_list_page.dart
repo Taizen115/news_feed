4d4c9930267d8919b9c34f374c04c5b6ceaa7ff2
taizen115 <taizen115@gmail.com>
2023-08-09 12:26:59 +0900

    文法エラーが出ているのですが、対応策がわかりません。

--- lib/view/screens/pages/news_list_page.dart
+++ lib/view/screens/pages/news_list_page.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 
-import '../../components/search_bar.dart';
-
 class NewsListPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
